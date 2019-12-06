5ddac97f198d0c168ff52ec9d215cf24e7e6eea1
Philippe Gauthier <philippe.gauthier@deuxpi.ca>
2019-12-05 20:18:45 -0500

    Day 5

--- intcode.js
+++ intcode.js
@@ -3,23 +3,74 @@ const fs = require('fs')
 const Intcode = class {
   constructor (program) {
     this.program = program
+    this.reset()
+  }
+
+  reset () {
     this.programCounter = 0
+    this.input = 1
   }
 
   executeNext () {
-    const opcode = this.fetch(this.programCounter)
+    const { opcode, modes } = this.parseInstruction(this.load(this.programCounter))
     const impl = {
       1: () => {
-        const arg1 = this.fetch(this.programCounter + 1)
-        const arg2 = this.fetch(this.programCounter + 2)
-        this.memory[this.fetch(this.programCounter + 3)] = this.fetch(arg1) + this.fetch(arg2)
+        const arg1 = this.load(this.programCounter + 1)
+        const arg2 = this.load(this.programCounter + 2)
+        this.memory[this.load(this.programCounter + 3)] = this.load(arg1, modes[0]) + this.load(arg2, modes[1])
         this.programCounter += 4
         return true
       },
       2: () => {
-        const arg1 = this.fetch(this.programCounter + 1)
-        const arg2 = this.fetch(this.programCounter + 2)
-        this.memory[this.fetch(this.programCounter + 3)] = this.fetch(arg1) * this.fetch(arg2)
+        const arg1 = this.load(this.programCounter + 1)
+        const arg2 = this.load(this.programCounter + 2)
+        this.memory[this.load(this.programCounter + 3)] = this.load(arg1, modes[0]) * this.load(arg2, modes[1])
+        this.programCounter += 4
+        return true
+      },
+      3: () => {
+        this.memory[this.load(this.programCounter + 1)] = this.input
+        this.programCounter += 2
+        return true
+      },
+      4: () => {
+        this.output = this.load(this.load(this.programCounter + 1), modes[0])
+        this.programCounter += 2
+        return true
+      },
+      5: () => {
+        const arg1 = this.load(this.programCounter + 1)
+        const arg2 = this.load(this.programCounter + 2)
+        if (this.load(arg1, modes[0]) !== 0) {
+          this.programCounter = this.load(arg2, modes[1])
+        } else {
+          this.programCounter += 3
+        }
+        return true
+      },
+      6: () => {
+        const arg1 = this.load(this.programCounter + 1)
+        const arg2 = this.load(this.programCounter + 2)
+        if (this.load(arg1, modes[0]) === 0) {
+          this.programCounter = this.load(arg2, modes[1])
+        } else {
+          this.programCounter += 3
+        }
+        return true
+      },
+      7: () => {
+        const arg1 = this.load(this.programCounter + 1)
+        const arg2 = this.load(this.programCounter + 2)
+        const arg3 = this.load(this.programCounter + 3)
+        this.memory[arg3] = this.load(arg1, modes[0]) < this.load(arg2, modes[1]) ? 1 : 0
+        this.programCounter += 4
+        return true
+      },
+      8: () => {
+        const arg1 = this.load(this.programCounter + 1)
+        const arg2 = this.load(this.programCounter + 2)
+        const arg3 = this.load(this.programCounter + 3)
+        this.memory[arg3] = this.load(arg1, modes[0]) === this.load(arg2, modes[1]) ? 1 : 0
         this.programCounter += 4
         return true
       },
@@ -37,8 +88,21 @@ const Intcode = class {
     while (this.executeNext()) {}
   }
 
-  fetch (address) {
-    return this.memory[address]
+  parseInstruction (instruction) {
+    const modes = [
+      Math.floor((instruction % 1000) / 100),
+      Math.floor((instruction % 10000) / 1000),
+      Math.floor((instruction % 100000) / 10000)
+    ]
+    return { opcode: instruction % 100, modes: modes }
+  }
+
+  load (param, mode) {
+    if (mode === 1) {
+      return param
+    } else {
+      return this.memory[param]
+    }
   }
 
   get program () {
@@ -59,7 +123,15 @@ if (require.main === module) {
     const intcode = new Intcode(program)
     intcode.setInputs(31, 46)
     intcode.execute()
-    console.log(intcode.fetch(0))
+  })
+
+  fs.readFile('./day_5_input', 'utf8', (err, data) => {
+    if (err) throw err
+    const program = data.trim()
+    const intcode = new Intcode(program)
+    intcode.input = 5
+    intcode.execute()
+    console.log(intcode.output)
   })
 }
 
